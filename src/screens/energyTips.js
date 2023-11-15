a279ada82e48e2bf6b9c9a9a5c9e1178608df96e
Edvanderson S.B Abreu <edvandersonsbabreu@gmail.com>
2023-11-15 16:10:44 -0300

    update

--- src/screens/energyTips.js
+++ src/screens/energyTips.js
@@ -8,11 +8,16 @@ export function EnergyTips(){
     "Desligue aparelhos da tomada quando não estiverem em uso",
     "Utilize eletrodomésticos com selo de eficiência energética",
     "Aproveite a luz natural e diminua o uso de luzes artificiais",
+    "Use aparelhos elétricos, como máquinas de lavar roupa e lava-louças, apenas com cargas completas para otimizar o uso de energia.",
+    "Troque telas tradicionais por telas de LED, que consomem menos energia.",
+    "Desconecte Carregadores Inativos.",
+    "Aproveite a Ventilação Natural.",
+    "Desligue Luzes ao Sair de um Ambiente."
     // Adicione mais dicas conforme necessário
   ];
 
   const usefulLinks = [
-    { title: 'Dicas de Economia - Site 1', url: 'https://www.exemplo.com/dicas-economia' },
+    { title: 'Embracon', url: 'https://www.embracon.com.br/blog/14-dicas-de-economia-para-colocar-em-pratica' },
     { title: 'Energia Sustentável - Site 2', url: 'https://www.exemplo.com/energia-sustentavel' },
     // Adicione mais links conforme necessário
   ];
@@ -30,15 +35,7 @@ export function EnergyTips(){
         ))}
     </ScrollView>
 
-    <Text style={styles.linksTitle}>Sites Úteis:</Text>
     
-    <View style={styles.linksContainer}>
-        {usefulLinks.map((link, index) => (
-            <TouchableOpacity key={index} onPress={() => alert(`Link para: ${link.url}`)}>
-                <Text style={styles.link}>{link.title}</Text>
-            </TouchableOpacity>
-        ))}
-    </View>
   </View>
   );
 };
